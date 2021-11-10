0c13cb03298fd568c238f8f7a5ef450a42613df1
LAP12196-local <LAP12196-local@10.79.9.71>
2021-11-10 22:30:10 +0700

    Create new Repository

---
+++ selenium/javaBasic/Check_URL.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+public class Check_URL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String projectPath = System.getProperty("user.dir");
+		System.out.println(projectPath);
+	}
+
+}
